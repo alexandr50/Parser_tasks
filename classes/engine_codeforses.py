dec603513730dcd22ccbe3783dbca66068be17e5
alexandr <dmitrymaslov2016@mai.ru>
2023-08-31 23:48:14 +0300

    add docstrings

--- classes/engine_codeforses.py
+++ classes/engine_codeforses.py
@@ -7,10 +7,14 @@ class EngineCF:
     URL = 'https://codeforces.com/api/problemset.problems'
 
     def get_request_for_problems(self) -> dict:
+        """Возвращает данные по ключу problems"""
+
         response = requests.get(self.URL)
         return response.json()['result']['problems']
 
     def get_check_new_task(self):
+        """Возвращает все уникальные номера задач"""
+
         lst = []
         response = self.get_request_for_problems()
         for item in response:
@@ -19,10 +23,14 @@ class EngineCF:
         return lst
 
     def get_request_for_problemstatistic(self) -> dict:
+        """Возвращает данные по ключу problemStatistics"""
+
         response = requests.get(self.URL)
         return response.json()['result']['problemStatistics']
 
     def get_content_from_problems(self, content: dict) -> list:
+        """Возвращает данные в списке по одной задаче  по ключу problems"""
+
         number = f"{content.get('contestId')}{content.get('index')}"
         name = content.get('name')
         rating = content.get('rating')
@@ -35,11 +43,14 @@ class EngineCF:
         return [number, name, theme[0], rating]
 
     def get_content_from_problemstatistic(self, content: dict) -> list:
+        """Возвращает данные в списке по одной задаче  по ключу problemStatistics"""
+
         count_solutions = content.get('solvedCount')
         number = f"{content.get('contestId')}{content.get('index')}"
         return [number, count_solutions]
 
     def get_result_data(self):
+        """Возвращает список кортежей задач для записи в бд"""
         result_data_from_problems = []
         result_data_from_problemstatistics = []
         for item in self.get_request_for_problems():
